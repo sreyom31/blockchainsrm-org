936be7a6fae4bac7786d597753f710c472cdbbfd
sreyom31 <sreyom31@gmail.com>
2022-03-27 14:09:25 +0530

    sass setup

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,10 @@
+import styles from "../styles/Home.module.scss"
+
 export default function Home() {
   return (
-    <h1 className="text-blue-500">
-      Hello world!
-    </h1>
-  )
+    <>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <p className={styles.para}>Scss is also working</p>
+    </>
+  );
 }
