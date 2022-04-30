96df0780a3cb445e4cf59c5049f443d7dcfd61f1
sreyom31 <sreyom31@gmail.com>
2022-04-30 21:16:59 +0530

    team page

---
+++ pages/team.tsx
@@ -0,0 +1,45 @@
+import Navbar from "../components/navbar";
+
+const Team = () => {
+    return (
+        <div className="overflow-hidden bg-bgteam bg-fixed relative">
+            <Navbar />
+            <div>
+                <div className="border-2 mt-5 py-10">
+                    <p className="uppercase text-white text-center text-4xl font-bold tracking-wide">Meet The Team</p>
+                </div>
+                <div className="border-2 text-white flex flex-wrap justify-center">
+                    <div className="border-2 my-8 mx-16 p-8 overflow-hidden ">
+                        <img className="rounded-full w-52 mx-auto" src="https://images.unsplash.com/photo-1597589827317-4c6d6e0a90bd?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OXx8c3F1YXJlJTIwc2hhcGV8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60" alt="" />
+                        <p className="my-2 text-4xl text-center">Sreyom Sresaan</p>
+                        <p className="my-2 text-xl text-center">Designation</p>
+                    </div>
+                    <div className="border-2 my-8 mx-16 p-8 overflow-hidden">
+                        <img className="rounded-full w-52 mx-auto" src="https://images.unsplash.com/photo-1597589827317-4c6d6e0a90bd?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OXx8c3F1YXJlJTIwc2hhcGV8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60" alt="" />
+                        <p className="my-2 text-4xl text-center">Sreyom Sresaan</p>
+                        <p className="my-2 text-xl text-center">Designation</p>
+                    </div>
+                </div>
+                <div className="border-2 text-white flex flex-wrap justify-center">
+                    <div className="border-2 my-8 mx-8 p-8 overflow-hidden">
+                        <img className="rounded-full w-52 mx-auto" src="https://images.unsplash.com/photo-1597589827317-4c6d6e0a90bd?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OXx8c3F1YXJlJTIwc2hhcGV8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60" alt="" />
+                        <p className="my-2 text-4xl text-center">Sreyom Sresaan</p>
+                        <p className="my-2 text-xl text-center">Designation</p>
+                    </div>
+                    <div className="border-2 my-8 mx-8 p-8 overflow-hidden">
+                        <img className="rounded-full w-52 mx-auto" src="https://images.unsplash.com/photo-1597589827317-4c6d6e0a90bd?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OXx8c3F1YXJlJTIwc2hhcGV8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60" alt="" />
+                        <p className="my-2 text-4xl text-center">Sreyom Sresaan</p>
+                        <p className="my-2 text-xl text-center">Designation</p>
+                    </div>
+                    <div className="border-2 my-8 mx-8 p-8 overflow-hidden">
+                        <img className="rounded-full w-52 mx-auto" src="https://images.unsplash.com/photo-1597589827317-4c6d6e0a90bd?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OXx8c3F1YXJlJTIwc2hhcGV8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60" alt="" />
+                        <p className="my-2 text-4xl text-center">Sreyom Sresaan</p>
+                        <p className="my-2 text-xl text-center">Designation</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+};
+
+export default Team;
