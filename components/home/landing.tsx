8b319af0196cbbe69cb871b795e61924372bbf7b
sreyom31 <sreyom31@gmail.com>
2022-05-01 17:34:53 +0530

    format code

--- components/home/landing.tsx
+++ components/home/landing.tsx
@@ -10,15 +10,24 @@ const Landing = () => {
         <div className="my-10 md:my-0 md:w-1/2 text-white xl:p-8 2xl:p-16 flex items-center">
           <div className="box m-8 md:m-0 p-8 xl:p-8 2xl:p-12 border-2">
             <div className="mb-4 lg:mb-8">
-              <p className="text-4xl md:text-2xl lg:text-4xl xl:text-6xl font-bold">Cool tagline goes here</p>
+              <p className="text-4xl md:text-2xl lg:text-4xl xl:text-6xl font-bold">
+                Cool tagline goes here
+              </p>
             </div>
             <div className="lg:my-8 lg:py-4">
               <p className="text-xl md:text-lg lg:text-xl xl:text-2xl">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores recusandae a repellendus quis, in quo omnis suscipit ut quos facere vitae facilis adipisci placeat quae numquam veritatis quasi ipsa. Rem cumque earum facere hic impedit enim nam iure nesciunt quibusdam, in architecto repudiandae nisi qui quod totam aperiam ipsam soluta.
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Asperiores recusandae a repellendus quis, in quo omnis suscipit
+                ut quos facere vitae facilis adipisci placeat quae numquam
+                veritatis quasi ipsa. Rem cumque earum facere hic impedit enim
+                nam iure nesciunt quibusdam, in architecto repudiandae nisi qui
+                quod totam aperiam ipsam soluta.
               </p>
             </div>
             <div className="mt-4 lg:mt-0">
-              <button className="p-2 px-4 outline-none border-2 border-secondary rounded-full font-semibold transition ease-out delay-100 hover:border-primary flex">Explore <ArrowRight className="ml-4" /> </button>
+              <button className="p-2 px-4 outline-none border-2 border-secondary rounded-full font-semibold transition ease-out delay-100 hover:border-primary flex">
+                Explore <ArrowRight className="ml-4" />{' '}
+              </button>
             </div>
           </div>
         </div>
