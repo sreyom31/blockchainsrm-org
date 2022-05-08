96e870d6ef1aad2997ea19becf43ae0806323afe
sreyom31 <sreyom31@gmail.com>
2022-05-08 16:03:09 +0530

    about logo positioning

--- components/home/about.tsx
+++ components/home/about.tsx
@@ -3,7 +3,8 @@ const Fade = require('react-reveal/Fade');
 
 const About = () => {
   return (
-    <div className="grid auto-cols-auto lg:grid-cols-2 gap-20 xl:gap-40 p-10 lg:p-12 xl:p-16 2xl:p-24 mt-24">
+    <div className="grid auto-cols-auto lg:grid-cols-2 gap-20 xl:gap-40 px-10 lg:px-12 xl:px-16 2xl:px-24 pt-10 lg:pt-12 xl:pt-16 2xl:pt-24 pb-96 mt-24 relative">
+      <img src="/images/mvimg.svg" className="absolute bottom-14 left-[50%] translate-x-[-50%]" alt="" />
       <div className="box p-6 text-white transform duration-300 hover:scale-110">
         <Fade top>
           <p className="text-center font-bold text-4xl md:text-2xl lg:text-4xl mb-4">
