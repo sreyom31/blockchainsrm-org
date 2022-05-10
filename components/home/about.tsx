b42e5eb4a8aeb6b60ec6667a6a54d39e49b6f214
agarwalviraj <viraj.ag007@gmail.com>
2022-05-10 22:15:45 +0530

    add animation

--- components/home/about.tsx
+++ components/home/about.tsx
@@ -2,14 +2,17 @@ import Image from 'next/image';
 import Card from '../shared/card';
 import Fade from 'react-reveal/Fade';
 
-// grid auto-cols-auto lg:grid-cols-2 gap-20 xl:gap-40 px-10 lg:px-12 xl:px-16 2xl:px-24 pt-10 lg:pt-12 xl:pt-16 2xl:pt-24 pb-96 mt-24
+import Bubbles from '../../public/images/mvimg.svg';
+import { ReactSVG } from 'react-svg';
+
 const About = () => {
   return (
     <div className="flex flex-col md:flex-row gap-12 lg:gap-32 w-full items-center justify-center mx-auto pb-96 mt-32 relative">
-      <img
-        src="/images/mvimg.svg"
-        className="absolute bottom-14 left-[50%] translate-x-[-50%]"
-      />
+      <div className="mvimg absolute bottom-14 left-[50%] translate-x-[-50%]">
+        {/* <Image width={269} height={452} src="/images/mvimg.svg" /> */}
+        <ReactSVG src="/images/mvimg.svg" />
+        {/* <Bubbles /> */}
+      </div>
       <Card className="max-w-2xl">
         <Fade top>
           <h2 className="text-center font-bold">Our Mission</h2>
