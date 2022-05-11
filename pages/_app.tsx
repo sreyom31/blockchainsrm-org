d63dc81601d40cd90962c5b57f9ff9d5943cc10a
sreyom31 <sreyom31@gmail.com>
2022-05-11 19:03:23 +0530

    seo

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -10,9 +10,55 @@ function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" type="image/x-icon" href="/images/logo.ico" />
-        <title>BLOCKCHAINSRM</title>
+        <title>Blockchain Club SRM</title>
+        <meta name="title" content="Blockchain Club SRM" />
+        <meta
+          name="description"
+          content="Pioneer student-led blockchain-centered community to foster young and inquisitive minds, that believe in implementing concepts of Web3 and Decentralization while educating students about Fundamentals and technical aspects of Trading"
+        />
+
+        <meta
+          name="keywords"
+          content="blockchain club srm blockchainsrm blockchainclubsrm clubsrm blockchainclub"
+        />
+        <meta name="robots" content="index, follow" />
+        <meta httpEquiv="Content-Type" content="text/html; charset=utf-8" />
+        <meta name="language" content="English" />
+        <meta name="revisit-after" content="1 days" />
+
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://www.blockchainsrm.org/" />
+        <meta property="og:title" content="Blockchain Club SRM" />
+        <meta
+          property="og:description"
+          content="Pioneer student-led blockchain-centered community to foster young and inquisitive minds, that believe in implementing concepts of Web3 and Decentralization while educating students about Fundamentals and technical aspects of Trading"
+        />
+        <meta
+          property="og:image"
+          content="https://i.postimg.cc/XqmqP0GN/officialsmall.png"
+        />
+        <meta property="og:locale" content="en_GB" />
+        <meta property="og:site_name" content="Blockchain Club SRM" />
+
+        <meta property="twitter:card" content="summary_large_image" />
+        <meta property="twitter:url" content="https://www.blockchainsrm.org/" />
+        <meta property="twitter:title" content="Blockchain Club SRM" />
+        <meta
+          property="twitter:description"
+          content="Pioneer student-led blockchain-centered community to foster young and inquisitive minds, that believe in implementing concepts of Web3 and Decentralization while educating students about Fundamentals and technical aspects of Trading"
+        />
+        <meta
+          property="twitter:image"
+          content="https://i.postimg.cc/XqmqP0GN/officialsmall.png"
+        />
+        <meta name="twitter:image:alt" content="Blockchain Club SRM" />
+
+        <link rel="icon" href="https://i.postimg.cc/zXkXWWpX/official.png" />
+        <link rel="canonical" href="https://www.blockchainsrm.org/" />
+        <link
+          rel="apple-touch-icon"
+          href="https://i.postimg.cc/zXkXWWpX/official.png"
+        />
       </Head>
       <Navbar />
       <Component {...pageProps} />
